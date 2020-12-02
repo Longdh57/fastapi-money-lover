96120b749c33e87011f210d4e414eec35e802e42
Long Dao <long.daohai4894@gmail.com>
2020-12-03 00:08:30 +0700

    add list url cors and fix dockerfile

--- app/core/config.py
+++ app/core/config.py
@@ -8,11 +8,15 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
+def get_list(text):
+    return [item.strip() for item in text.split(',')]
+
+
 class Settings(BaseSettings):
     API_PREFIX = '/api/v1'
     PROJECT_NAME = os.getenv('PROJECT_NAME', 'Money Lover')
     SECRET_KEY = os.getenv('SECRET_KEY', '')
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ["http://localhost:9000"]
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = get_list(os.getenv('FONTEND_URL'))
     DATABASE_URL = os.getenv('SQL_DATABASE_URL', '')
 
 
